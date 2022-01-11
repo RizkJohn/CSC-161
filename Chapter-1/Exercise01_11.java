348963974be67b51b2cf38d1c4515f936b5feed4
John Rizkalla <79679734+RizkJohn@users.noreply.github.com>
2022-01-10 21:04:56 -0500

    Add files via upload

---
+++ Chapter-1/Exercise01_11.java
@@ -0,0 +1,22 @@
+package chapter_1;
+
+public class Exercise01_11 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Population after 1 year: ");
+		System.out.println(
+				312032486 + 365 * 24 * 60 * 60 / 7.0 - 365.0 * 24 * 60 * 60 / 13.0 + 365 * 24 * 60 * 60 / 45.0);
+		System.out.print("Population after 2 years: ");
+		System.out.println(312032486 + 2 * 365 * 24 * 60 * 60 / 7.0 - 2 * 365.0 * 24 * 60 * 60 / 13.0
+				+ 2 * 365 * 24 * 60 * 60 / 45.0);
+		System.out.print("Population after 3 years: ");
+		System.out.println(312032486 + 3 * 365 * 24 * 60 * 60 / 7.0 - 3 * 365.0 * 24 * 60 * 60 / 13.0
+				+ 3 * 365 * 24 * 60 * 60 / 45.0);
+		System.out.print("Population after 4 years: ");
+		System.out.println(312032486 + 4 * 365 * 24 * 60 * 60 / 7.0 - 4 * 365.0 * 24 * 60 * 60 / 13.0
+				+ 4 * 365 * 24 * 60 * 60 / 45.0);
+		System.out.print("Population after 5 years: ");
+		System.out.println(312032486 + 5 * 365 * 24 * 60 * 60 / 7.0 - 5 * 365.0 * 24 * 60 * 60 / 13.0
+				+ 5 * 365 * 24 * 60 * 60 / 45.0);
+	}
+}
